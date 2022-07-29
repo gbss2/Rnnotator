57f41d520efe4872d78b141d64c06cfe9085caf4
gbss2 <jwojwo@gmail.com>
2022-07-29 17:47:11 -0300

    t1

---
+++ R/hgnc_rest.R
@@ -0,0 +1,17 @@
+#' HGNC REST Access
+#’
+#' @author Giordano Bruno Soares-Souza
+#' @param gene : Gene Name.
+#’
+#' @return A character list.
+#' @export
+#’
+#' @examples
+#'
+#' res <- hgnc_rest("CYBB")
+#'
+hgnc_rest <- function(){
+
+
+
+}
