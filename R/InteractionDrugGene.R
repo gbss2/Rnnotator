74f84ceb94339b66b8954357935a011da3fbd1fb
aviegas93 <110051343+aviegas93@users.noreply.github.com>
2022-10-07 15:08:09 -0300

    teste

--- R/InteractionDrugGene.R
+++ R/InteractionDrugGene.R
@@ -1,36 +1,88 @@
-#' Show the interactions between a given set of drugs and a gene names
+#' Show the interactions between a given set of drugs and a genes names
 #’
 #' @author Adriano Viegas
-#' @param drugs : A set of drugs (required)*.
-#' @param itype : Interaction type - can be: inhibitor or activator (optional)*.
-#’
-#' @return A character list.
+#' @param drug : (required) - A set of drugs
+#' @param typeReturn : (optional) - "data_frame" (default) or "'table" (first: use library(gt) and gt(res) - res as function return).
+#' @param interaction_sources : (optional) - Can be : "DrugBank","PharmGKB","TALC","TEND","TTD"
+#' @param interaction_types : (optional) - Can be: "activator", "inhibitor", "unknown" - (For some researches, params like interaction_types don't work properly, you should use interaction_source instead)
+#' @param anti_neoplastic : (optional) - Can be: "true"
+#' @param clinically_actionable : (optional) - Can be: "true"
+#' @param gene_categories : (optional) - Can be: "KINASE", "DNA REPAIR", "TUMOR SUPPRESSOR"
+#'
+#' @return A data frame or gt table.
 #' @export
 #’
 #' @examples
 #'
-#' res <- interactionDrugGene("VATALANIB", "inhibitor")
-#' res
-#' VATALANIB - KDR
-#' VATALANIB - CSF1R
+#' res <- interactionDrugGene(drug='FLT1,FLT3', interaction_sources='TALC', anti_neoplastic='true')
+#'
+interactionDrugGene <- function(drug=drug, typeReturn='data_frame', interaction_sources=NULL, interaction_types=NULL, anti_neoplastic=NULL, gene_categories=NULL, clinically_actionable=NULL) {
+  url = (paste("https://dgidb.org/api/v2/interactions.json?drugs=", drug, sep=''))
 
-interactionDrugGene = function(drugs, itype) {
-  if(missing(itype)){
-    out = GET(paste("https://dgidb.org/api/v2/interactions.json?drugs=", drugs, sep=""), content_type("application/json"))
+  if(!is.null(interaction_sources)){
+    url = paste(url, '&interaction_sources=', interaction_sources, sep='')
   }
-  else{
-    out = GET(paste("https://dgidb.org/api/v2/interactions.json?drugs=", drugs,"&interaction_types=", itype, sep=""), content_type("application/json"))
+
+  if(!is.null(interaction_types)){
+    url = paste(url, '&interaction_types=', interaction_types, sep='')
   }
-  out = head(fromJSON(toJSON(content(out))))
-  cnt = 1
-  res = list()
-  #teste
-  #teste
-  #teste
-  #teste
-  for(i in out$matchedTerms$drugName){
-    res[i] = list(out$matchedTerms$interactions[[cnt]]$geneName)
-    cnt = cnt + 1
+
+  if(!is.null(anti_neoplastic)){
+    url = paste(url, '&anti_neoplastic=', anti_neoplastic, sep='')
   }
-  return(res)
+
+  if(!is.null(gene_categories)){
+    url = paste(url, '&gene_categories=', gene_categories, sep='')
+  }
+
+  if(!is.null(clinically_actionable)){
+    url = paste(url, '&clinically_actionable=', clinically_actionable, sep='')
+  }
+  response = httr::GET(url)
+
+  rJs <- httr::content(response, as="text")
+  out <- jsonlite::fromJSON(rJs)
+
+  data <- list()
+
+  for(i in 1:length(out$matchedTerms$interactions)){
+
+    lInteract = length(out$matchedTerms$interactions[[i]]$geneName)
+
+    drugName <- replicate(lInteract, out$matchedTerms$drugName[[i]])
+    data$drugName <- c(data$drugName, drugName)
+    conceptId <- replicate(lInteract, out$matchedTerms$conceptId[[i]])
+
+    geneName <- c()
+    interactionTypes <- c()
+
+    geneName <- out$matchedTerms$interactions[[i]]$geneName
+    interactionTypes <- out$matchedTerms$interactions[[i]]$interactionTypes
+
+    data$geneName <- c(data$geneName, geneName)
+    data$interactionTypes <- sapply(c(data$interactionTypes, interactionTypes), paste0, collapse=';')
+    data$conceptId <- c(data$conceptId, conceptId)
+
+  }
+
+  if(typeReturn == 'data_frame'){
+    dataFrame <- as.data.frame(data)
+    return(dataFrame)
+  }
+
+  if(typeReturn == 'table'){
+    link = sub('chembl:', 'https://www.ebi.ac.uk/chembl/compound_report_card/', data$conceptId)
+
+    df <- tibble::tibble(
+      drugName=data$drugName,
+      geneName=data$geneName,
+      interactionTypes=data$interactionTypes,
+      link)
+
+    # using html
+
+      dplyr::mutate(df,
+        link = glue::glue("[website]({link})"),
+        link = purrr::map(link, gt::md)) -> df
+  }
 }
--- R/interactionGeneDrug.R
+++ R/interactionGeneDrug.R
@@ -1,32 +1,88 @@
-#' Show the interactions between a given set of gene and a drug names
+#' Show the interactions between a given set of genes and a drugs names
 #’
 #' @author Adriano Viegas
-#' @param genes : A set of genes (required)*.
-#' @param itype : Interaction type - can be: inhibitor or activator (optional)*.
-#’
-#' @return A character list.
+#' @param gene : (required) - A set of genes.
+#' @param typeReturn : (optional) - "data_frame" (default) or "table" (first: use library(gt) and gt(res) - res as function return).
+#' @param interaction_sources : (optional) - Can be : "DrugBank","PharmGKB","TALC","TEND","TTD"
+#' @param interaction_types : (optional) - Can be: "activator", "inhibitor", "unknown" - (For some researches, params like interaction_types don't work properly, you should use interaction_source instead)
+#' @param anti_neoplastic : (optional) - Can be: "true"
+#' @param clinically_actionable : (optional) - Can be: "true"
+#' @param gene_categories : (optional) - Can be: "KINASE", "DNA REPAIR", "TUMOR SUPPRESSOR"
+#'
+#' @return A data frame or gt table.
 #' @export
 #’
 #' @examples
 #'
-#' res <- interactionGeneDrug("FLT1", "inhibitor")
-#' res
-#' FLT1 - DOVITINIB
-#' FLT1 - LINIFANIB
+#' res <- interactionGeneDrug(gene='FLT1,FLT3', interaction_sources='TALC', anti_neoplastic='true')
 #'
-interactionGeneDrug = function(genes, itype) {
-  if(missing(itype)){
-    out = GET(paste("https://dgidb.org/api/v2/interactions.json?genes=", genes, sep=""), content_type("application/json"))
+interactionGeneDrug <- function(gene=gene, typeReturn='data_frame', interaction_sources=NULL, interaction_types=NULL, anti_neoplastic=NULL, gene_categories=NULL, clinically_actionable=NULL) {
+  url = (paste("https://dgidb.org/api/v2/interactions.json?genes=", gene, sep=''))
+
+  if(!is.null(interaction_sources)){
+    url = paste(url, '&interaction_sources=', interaction_sources, sep='')
   }
-  else{
-    out = GET(paste("https://dgidb.org/api/v2/interactions.json?genes=", genes,"&interaction_types=", itype, sep=""), content_type("application/json"))
+
+  if(!is.null(interaction_types)){
+    url = paste(url, '&interaction_types=', interaction_types, sep='')
   }
-  out = head(fromJSON(toJSON(content(out))))
-  cnt = 1
-  res = list()
-  for(i in out$matchedTerms$geneName){
-    res[i] = list(out$matchedTerms$interactions[[cnt]]$drugName)
-    cnt = cnt + 1
+
+  if(!is.null(anti_neoplastic)){
+    url = paste(url, '&anti_neoplastic=', anti_neoplastic, sep='')
   }
-  return(res)
+
+  if(!is.null(gene_categories)){
+    url = paste(url, '&gene_categories=', gene_categories, sep='')
+  }
+
+  if(!is.null(clinically_actionable)){
+    url = paste(url, '&clinically_actionable=', clinically_actionable, sep='')
+  }
+  response = httr::GET(url)
+
+  rJs <- httr::content(response, as="text")
+  out <- jsonlite::fromJSON(rJs)
+
+  data <- list()
+
+  for(i in 1:length(out$matchedTerms$interactions)){
+
+    lInteract = length(out$matchedTerms$interactions[[i]]$drugName)
+
+    geneName <- replicate(lInteract, out$matchedTerms$geneName[[i]])
+    data$geneName <- c(data$geneName, geneName)
+
+    drugName <- c()
+    interactionTypes <- c()
+
+    drugName <- out$matchedTerms$interactions[[i]]$drugName
+    interactionTypes <- out$matchedTerms$interactions[[i]]$interactionTypes
+    drugConceptId <- out$matchedTerms$interactions[[i]]$drugConceptId
+
+    data$drugName <- c(data$drugName, drugName)
+    data$interactionTypes <- sapply(c(data$interactionTypes, interactionTypes), paste0, collapse=';')
+    data$drugConceptId <- c(data$drugConceptId, drugConceptId)
+
+  }
+
+  if(typeReturn == 'data_frame'){
+    dataFrame <- as.data.frame(data)
+    return(dataFrame)
+  }
+
+  if(typeReturn == 'table'){
+    link = sub('chembl:', 'https://www.ebi.ac.uk/chembl/compound_report_card/', data$drugConceptId)
+
+    df <- tibble::tibble(
+      nameGane=data$geneName,
+      drugName=data$drugName,
+      interactionTypes=data$interactionTypes,
+      link)
+
+    # using html
+
+      dplyr::mutate(df,
+        link = glue::glue("[website]({link})"),
+        link = purrr::map(link, gt::md)) -> df
+  }
 }
